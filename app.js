d0ff3f97904de3b17e9c86b673b2418d511768ef
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2025-07-05 13:46:20 +0530

    Index route and database initialization done

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const ejs = require("ejs");
+const Listing = require("./models/listing");
+const path = require("path");
+
+const MONGO_URL = "mongodb://localhost:27017/wanderlust";
+
+main().then(() => {
+    console.log("connected to db");
+})
+.catch((err) => {
+    console.log(err);
+});
+
+async function main() {
+    await mongoose.connect(MONGO_URL);
+}
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.send("Hi, I am root");
+});
+
+app.get("/listings", async (req, res) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+});
+
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My New Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India",
+//     });
+
+//     await sampleListing.save();
+//     console.log("sample was saved");
+//     res.send("successful testing");
+// });
+
+app.listen(8080, () => {
+    console.log("server is listening to port 8080");
+});
+
