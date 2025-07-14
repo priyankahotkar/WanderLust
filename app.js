4238a376270f571cd0d3f5c587b322a25d2691cc
priyankahotkar <160253899+priyankahotkar@users.noreply.github.com>
2025-07-14 12:13:50 +0530

    Added boilerplate code

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const ejs = require("ejs");
 const Listing = require("./models/listing");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const MONGO_URL = "mongodb://localhost:27017/wanderlust";
 
@@ -23,6 +24,8 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.send("Hi, I am root");
